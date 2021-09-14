fabcef424eb775cef81d25f823f67ab4871e0bd4
Parth Patel <parth6796@yahoo.com>
2021-09-14 20:35:53 +0100

    cleaned the profile pic and location up with a ternary

--- src/components/ProfileInfo/index.js
+++ src/components/ProfileInfo/index.js
@@ -7,11 +7,13 @@ const ProfileInfo = () => {
 
 	return (
 		<>
-			<img src={user.avatar_url} alt="User's Profile Picture" />
+			<div>
+				{user.avatar_url ? <img src={user.avatar_url} alt="User's Profile Picture" /> : <p></p>}
+			</div>
 			<a href={user.html_url} target="_blank">
 				<h2>{user.login}</h2>
 			</a>
-			<h3>{user.location ? user.location : 'Unknown Location'}</h3>
+			<h3>{user.location ? user.location : ''}</h3>
 		</>
 	);
 };
