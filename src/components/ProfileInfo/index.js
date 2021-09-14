3e3afd10235b21eb7546b2ffb4633c1dac278bd9
Parth Patel <parth6796@yahoo.com>
2021-09-14 21:11:58 +0100

    changed layout

--- src/components/ProfileInfo/index.js
+++ src/components/ProfileInfo/index.js
@@ -7,13 +7,13 @@ const ProfileInfo = () => {
 
 	return (
 		<>
+			<h2>{user.login}</h2>
 			<div>
 				{user.avatar_url ? <img src={user.avatar_url} alt="User's Profile Picture" /> : <p></p>}
 			</div>
-			<a href={user.html_url} target="_blank">
-				<h2>{user.login}</h2>
-			</a>
+			<a href={user.html_url} target="_blank"></a>
 			<h3>{user.location ? user.location : ''}</h3>
+			{user.login ? <h2> {user.login}'s Repos</h2> : ''}
 		</>
 	);
 };
