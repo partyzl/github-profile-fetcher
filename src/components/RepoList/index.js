c80550cc70f46e5883cf9e35624ba4c9cc5737b9
Parth Patel <parth6796@yahoo.com>
2021-09-14 21:42:29 +0100

    mapping the results of the repos

--- src/components/RepoList/index.js
+++ src/components/RepoList/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const RepoList = () => {
+	const [repoList, setRepoList] = useState([]);
 	const { results } = useSelector((state) => state.reposReducer);
 
+	useEffect(() => {
+		const renderRepos = () => {
+			const userRepos = results.map((repo) => <Link to={`/user${repo.name}`}> {repo.name}</Link>);
+			setRepoList(userRepos);
+		};
+		renderRepos();
+	}, [results]);
+	//I think we need to add the below into the actions, unless you want to add them here
 	return (
-		<div>
-			<h2>`{results.login}'s Repos</h2>
-			<div className="repo-list"></div>
-		</div>
+		<>
+			<div className="repo-list">{repoList}</div>
+		</>
 	);
 };
 
@@ -18,4 +26,5 @@ html_url
 stargazers_count
 watchers_count
 forks_count
+language
 */
