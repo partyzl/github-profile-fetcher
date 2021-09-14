88b19ff14d1aba1c80030f808b82c61fb6b2391a
Parth Patel <parth6796@yahoo.com>
2021-09-14 17:51:00 +0100

    applied changes to router

---
+++ src/components/RepoList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RepoList = () => {
+	const { results } = useSelector((state) => state.reposReducer);
+
+	return {};
+};
+
+/*
+name
+html_url
+stargazers_count
+watchers_count
+forks_count
+*/
