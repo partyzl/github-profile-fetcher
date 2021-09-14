afc6ef639051190f332988eaf1f4a31230145838
Tobi Kuyoro <tobikuyo@gmail.com>
2021-09-14 16:57:54 +0100

    Further redux setup

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,22 @@
 import axios from 'axios';
-import { LOADING, SEARCH_USER_FAILURE, SEARCH_USER_SUCCESS } from './constants';
+import {
+	LOADING_REPO,
+	LOADING_USER,
+	SEARCH_REPO_FAILURE,
+	SEARCH_REPO_SUCCESS,
+	SEARCH_USER_FAILURE,
+	SEARCH_USER_SUCCESS,
+} from './constants';
 
 /**
- * Get information about a user's repositories and handle state accordingly
+ * Get information about a user
  * @param {string} username used to search for a specific user's information
  */
-export const getRepos = (username) => async (dispatch) => {
-	dispatch({ type: LOADING });
+export const getUserInfo = (username) => async (dispatch) => {
+	dispatch({ type: LOADING_USER });
 
 	try {
-		const data = await axios.get(`https://api.github.com/users/${username}/repos`);
+		const data = await axios.get(`https://api.github.com/users/${username}`);
 		dispatch({ type: SEARCH_USER_SUCCESS, payload: data });
 	} catch (error) {
 		dispatch({ type: SEARCH_USER_FAILURE, payload: error.message });
@@ -17,4 +24,18 @@ export const getRepos = (username) => async (dispatch) => {
 	}
 };
 
-getRepos('tobikuyo');
+/**
+ * Get information about a user's repositories and handle state accordingly
+ * @param {string} username used to search for a specific user's information
+ */
+export const getRepos = (username) => async (dispatch) => {
+	dispatch({ type: LOADING_REPO });
+
+	try {
+		const data = await axios.get(`https://api.github.com/users/${username}/repos`);
+		dispatch({ type: SEARCH_REPO_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({ type: SEARCH_REPO_FAILURE, payload: error.message });
+		console.error('Error getting user repos ', error);
+	}
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import reposReducer from './reducer';
+import rootReducer from './reducers';
 
-const store = createStore(reposReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
