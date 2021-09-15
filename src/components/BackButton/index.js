ab15b0c758a23e571a14c9e126a1d8abdb12cedc
Parth Patel <parth6796@yahoo.com>
2021-09-15 10:39:32 +0100

    added backbutton on repo data page

---
+++ src/components/BackButton/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+const BackButton = () => {
+	const history = useHistory();
+	return (
+		<>
+			<input id="back-btn" onClick={history.goBack} type="button" value="<"></input>
+		</>
+	);
+};
+
+export default BackButton;
