745182c20c70f0cd55da6ebc7b5b3544f58992ef
Tobi Kuyoro <tobikuyo@gmail.com>
2021-09-18 19:10:26 +0100

    Implement persistence to fix refresh issues

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistConfig = {
+	key: 'root',
+	storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistor = persistStore(store);
+
+export { store, persistor };
